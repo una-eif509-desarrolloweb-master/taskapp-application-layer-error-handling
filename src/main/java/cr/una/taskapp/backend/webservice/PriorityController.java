b40576282697a8f128c773db9b4ccf22ab9e50c1
Maikol Guzman Alan <mguzmana@akamai.com>
2020-09-28 17:48:18 -0600

    Minor changes
    Signed-off-by: Maikol Guzman Alan <mguzmana@akamai.com>

--- src/main/java/cr/una/taskapp/backend/webservice/PriorityController.java
+++ src/main/java/cr/una/taskapp/backend/webservice/PriorityController.java
@@ -98,7 +98,12 @@ public class PriorityController {
     @DeleteMapping("{id}")
     @ResponseBody
     public void deleteById(@PathVariable Long id) throws PriorityNotFoundException {
-        service.deleteById(id);
+        try {
+            service.deleteById(id);
+        } catch (PriorityNotFoundException ex) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Priority Not Found", ex);
+        }
     }
     
     /**
